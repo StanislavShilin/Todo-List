1757aa10d3b091e328918fda2d5876f001f74549
mrshikadancer <mrshikadancer@yandex.ru>
2020-01-08 23:25:13 +0300

    add css

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import ReactDom from 'react-dom';
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
 
 const App = () => {
     const todoList = [
         {label: 'Drink coffee', important: false, id: 1},
-        {label: 'Make eat', important: false, id: 2},
+        {label: 'Have a lunch', important: false, id: 2},
         {label: 'Go to the job', important: true, id: 3},
     ];
 
     return (
-      <div>
-        <AppHeader />
-        <SearchPanel />
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+
+        <div className="top-panel d-flex">
+            <SearchPanel />
+            <ItemStatusFilter />
+        </div>
+
         <TodoList todos={todoList}/>
       </div>
     );
